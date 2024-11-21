25841376a4069f5b95d7532f4d5a6855a09412b1
Accel-Academics <contactaccelacademics@gmail.com>
2024-11-21 13:02:11 -0600

    Add Games to games.json
    Also updated gameList.js to display all of the games.

--- gameList.js
+++ gameList.js
@@ -12,20 +12,68 @@ function fetchJSONData() {
       {
         // Build an side nav item for every object each array
         // HTML GAMES 
-        for (let x in jsonData.html) {
+        for (let x in jsonData.HTML) {
           gameListElement = document.createElement("a");
-          gameListElement.href = jsonData.html[x].link;
-          gameListElement.innerHTML = jsonData.html[x].title;
+          gameListElement.href = jsonData.HTML[x].link;
+          gameListElement.innerHTML = jsonData.HTML[x].title;
           document.getElementById("HTMLGameList").appendChild(gameListElement);
-          console.log("Created navItem: " + jsonData.html[x].title); // For Debugging
+          console.log("Created navItem: " + jsonData.HTML[x].title); // For Debugging
         }
         // FLASH GAMES
-        for (let x in jsonData.flash) {
+        for (let x in jsonData.FLASH) {
           gameListElement = document.createElement("a");
-          gameListElement.href = jsonData.flash[x].link;
-          gameListElement.innerHTML = jsonData.flash[x].title;
+          gameListElement.href = jsonData.FLASH[x].link;
+          gameListElement.innerHTML = jsonData.FLASH[x].title;
           document.getElementById("FlashGameList").appendChild(gameListElement);
-          console.log("Created navItem: " + jsonData.flash[x].title); // For Debugging
+          console.log("Created navItem: " + jsonData.FLASH[x].title); // For Debugging
+        }
+        // NDS GAMES
+        for (let x in jsonData.NDS) {
+          gameListElement = document.createElement("a");
+          gameListElement.href = jsonData.NDS[x].link;
+          gameListElement.innerHTML = jsonData.NDS[x].title;
+          document.getElementById("NDSGameList").appendChild(gameListElement);
+          console.log("Created navItem: " + jsonData.NDS[x].title); // For Debugging
+        }
+        // GBA GAMES
+        for (let x in jsonData.GBA) {
+          gameListElement = document.createElement("a");
+          gameListElement.href = jsonData.GBA[x].link;
+          gameListElement.innerHTML = jsonData.GBA[x].title;
+          document.getElementById("GBAGameList").appendChild(gameListElement);
+          console.log("Created navItem: " + jsonData.GBA[x].title); // For Debugging
+        }
+        // N64 GAMES
+        for (let x in jsonData.N64) {
+          gameListElement = document.createElement("a");
+          gameListElement.href = jsonData.N64[x].link;
+          gameListElement.innerHTML = jsonData.N64[x].title;
+          document.getElementById("N64GameList").appendChild(gameListElement);
+          console.log("Created navItem: " + jsonData.N64[x].title); // For Debugging
+        }
+        // NES GAMES
+        for (let x in jsonData.NES) {
+          gameListElement = document.createElement("a");
+          gameListElement.href = jsonData.NES[x].link;
+          gameListElement.innerHTML = jsonData.NES[x].title;
+          document.getElementById("NESGameList").appendChild(gameListElement);
+          console.log("Created navItem: " + jsonData.NES[x].title); // For Debugging
+        }
+        // SNES GAMES
+        for (let x in jsonData.SNES) {
+          gameListElement = document.createElement("a");
+          gameListElement.href = jsonData.SNES[x].link;
+          gameListElement.innerHTML = jsonData.SNES[x].title;
+          document.getElementById("SNESGameList").appendChild(gameListElement);
+          console.log("Created navItem: " + jsonData.SNES[x].title); // For Debugging
+        }
+        // JSDOS GAMES
+        for (let x in jsonData.JSDOS) {
+          gameListElement = document.createElement("a");
+          gameListElement.href = jsonData.JSDOS[x].link;
+          gameListElement.innerHTML = jsonData.JSDOS[x].title;
+          document.getElementById("JSDOSGameList").appendChild(gameListElement);
+          console.log("Created navItem: " + jsonData.JSDOS[x].title); // For Debugging
         }
       })
 }
