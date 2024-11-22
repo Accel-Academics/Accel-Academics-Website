d558576ebdd525131a23503935ee018df5e683a3
Accel-Academics <contactaccelacademics@gmail.com>
2024-11-22 13:38:14 -0600

    Minor fixes
    Create CSS variable for max height.
    Start the process of making a game window.
    Update a couple pages with latest nav menu.
    not sure what I haven't mentioned

--- gameList.js
+++ gameList.js
@@ -12,11 +12,12 @@ function fetchJSONData() {
       {
         let start = new Date().getTime()
         var count = 0;
-        // Build an side nav item for every object each array
+        var gameListElement;
+        // Build an side nav item for every object in each array
         // HTML GAMES 
         for (let x in jsonData.HTML) {
           gameListElement = document.createElement("a");
-          gameListElement.href = jsonData.HTML[x].link;
+          gameListElement.setAttribute("onClick", 'openGame("' + jsonData.HTML[x].link + '")');
           gameListElement.innerHTML = jsonData.HTML[x].title;
           document.getElementById("HTMLGameList").appendChild(gameListElement);
           // console.log("Created navItem: " + jsonData.HTML[x].title); // For Debugging
@@ -25,7 +26,7 @@ function fetchJSONData() {
         // FLASH GAMES
         for (let x in jsonData.FLASH) {
           gameListElement = document.createElement("a");
-          gameListElement.href = jsonData.FLASH[x].link;
+          gameListElement.setAttribute("onClick", 'openGame("' + jsonData.FLASH[x].link + '")');
           gameListElement.innerHTML = jsonData.FLASH[x].title;
           document.getElementById("FlashGameList").appendChild(gameListElement);
           // console.log("Created navItem: " + jsonData.FLASH[x].title); // For Debugging
@@ -34,7 +35,7 @@ function fetchJSONData() {
         // NDS GAMES
         for (let x in jsonData.NDS) {
           gameListElement = document.createElement("a");
-          gameListElement.href = jsonData.NDS[x].link;
+          gameListElement.setAttribute("onClick", 'openGame("' + jsonData.NDS[x].link + '")');
           gameListElement.innerHTML = jsonData.NDS[x].title;
           document.getElementById("NDSGameList").appendChild(gameListElement);
           // console.log("Created navItem: " + jsonData.NDS[x].title); // For Debugging
@@ -43,7 +44,7 @@ function fetchJSONData() {
         // GBA GAMES
         for (let x in jsonData.GBA) {
           gameListElement = document.createElement("a");
-          gameListElement.href = jsonData.GBA[x].link;
+          gameListElement.setAttribute("onClick", 'openGame("' + jsonData.GBA[x].link + '")');
           gameListElement.innerHTML = jsonData.GBA[x].title;
           document.getElementById("GBAGameList").appendChild(gameListElement);
           // console.log("Created navItem: " + jsonData.GBA[x].title); // For Debugging
@@ -52,7 +53,7 @@ function fetchJSONData() {
         // N64 GAMES
         for (let x in jsonData.N64) {
           gameListElement = document.createElement("a");
-          gameListElement.href = jsonData.N64[x].link;
+          gameListElement.setAttribute("onClick", 'openGame("' + jsonData.N64[x].link + '")');
           gameListElement.innerHTML = jsonData.N64[x].title;
           document.getElementById("N64GameList").appendChild(gameListElement);
           // console.log("Created navItem: " + jsonData.N64[x].title); // For Debugging
@@ -61,7 +62,7 @@ function fetchJSONData() {
         // NES GAMES
         for (let x in jsonData.NES) {
           gameListElement = document.createElement("a");
-          gameListElement.href = jsonData.NES[x].link;
+          gameListElement.setAttribute("onClick", 'openGame("' + jsonData.NES[x].link + '")');
           gameListElement.innerHTML = jsonData.NES[x].title;
           document.getElementById("NESGameList").appendChild(gameListElement);
           // console.log("Created navItem: " + jsonData.NES[x].title); // For Debugging
@@ -70,7 +71,7 @@ function fetchJSONData() {
         // SNES GAMES
         for (let x in jsonData.SNES) {
           gameListElement = document.createElement("a");
-          gameListElement.href = jsonData.SNES[x].link;
+          gameListElement.setAttribute("onClick", 'openGame("' + jsonData.SNES[x].link + '")');
           gameListElement.innerHTML = jsonData.SNES[x].title;
           document.getElementById("SNESGameList").appendChild(gameListElement);
           // console.log("Created navItem: " + jsonData.SNES[x].title); // For Debugging
@@ -79,7 +80,7 @@ function fetchJSONData() {
         // JSDOS GAMES
         for (let x in jsonData.JSDOS) {
           gameListElement = document.createElement("a");
-          gameListElement.href = jsonData.JSDOS[x].link;
+          gameListElement.setAttribute("onClick", 'openGame("' + jsonData.JSDOS[x].link + '")');
           gameListElement.innerHTML = jsonData.JSDOS[x].title;
           document.getElementById("JSDOSGameList").appendChild(gameListElement);
           // console.log("Created navItem: " + jsonData.JSDOS[x].title); // For Debugging
