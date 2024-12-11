4421186794deb884f03184e98a8f68a3108081a1
Accel-Academics <contactaccelacademics@gmail.com>
2024-12-11 11:04:34 -0600

    Frontend Overhaul

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
 /* 
   Add funtionality here
-  - Fullscreen games
+  - Fullscreen gs
   - Fetching link and assigning to iFrame
   - Expand / Colapse Side Nav Categories  
 */
 
-function openGame(gameLink) {
+function openg(gLink) {
   document.getElementById("content").innerHTML = "";
-  var gameiframe = document.createElement("iframe");
-  gameiframe.id = "gameiframe"
-  gameiframe.style.border = "none";
-  gameiframe.style.margin = "0";
-  gameiframe.width = "100%";
-  gameiframe.height = "98%";
-  gameiframe.src = gameLink;
-  document.getElementById("content").appendChild(gameiframe);
+  var giframe = document.createElement("iframe");
+  giframe.id = "giframe"
+  giframe.style.border = "none";
+  giframe.style.margin = "0";
+  giframe.width = "100%";
+  giframe.height = "98%";
+  giframe.src = gLink;
+  document.getElementById("content").appendChild(giframe);
 }
 
 /* Fullscreen Button Functionality */
@@ -25,18 +25,18 @@ btn.onclick=function(){
     win.focus()
   }
   else{ // Create a new window
-    if (document.getElementById("gameiframe").src) {
+    if (document.getElementById("giframe").src) {
       win = window.open();
       win.document.body.style.margin = "0";
       win.document.body.style.height = "100vh";
       
-      var gamewindow = win.document.createElement("iframe");
-      gamewindow.style.border = "none";
-      gamewindow.style.width = "100%";
-      gamewindow.style.height = "100%";
-      gamewindow.style.margin = "0";
-      gamewindow.src = document.getElementById("gameiframe").src;
-      win.document.body.appendChild(gamewindow);
+      var gwindow = win.document.createElement("iframe");
+      gwindow.style.border = "none";
+      gwindow.style.width = "100%";
+      gwindow.style.height = "100%";
+      gwindow.style.margin = "0";
+      gwindow.src = document.getElementById("giframe").src;
+      win.document.body.appendChild(gwindow);
       var a = setInterval( // Closed and prompt reopenning
         function() {
           if(win.closed){
